92fa04eba8afba5f44abeddb50e31bfd50886418
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-01 13:42:13 -0500

    Add credit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './components/HomePage';
+/*
+	Made by: GreatGameDota
+	https://github.com/GreatGameDota
+*/
 
 class App extends Component {
 	render () {
