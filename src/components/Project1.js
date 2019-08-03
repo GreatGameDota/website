bf9523c86b830a5179ddfd3903f6bd55402dadb5
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-02 21:19:01 -0500

    Add test download button

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+import styles from '../styles/Project1Styles';
 
-export default class Project1 extends Component {
-  render() {
-    return (
-      <div>
-        Project1
-      </div>
-    )
-  }
+class Project1 extends Component {
+	render () {
+		const { classes } = this.props;
+		return (
+			<div>
+				Project1
+        <br></br>
+				<Button variant='contained' className={classes.button}>
+					<a href='https://github.com/GreatGameDota/my-website/archive/master.zip'>Download</a>
+				</Button>
+			</div>
+		);
+	}
 }
+export default withStyles(styles)(Project1);
---
+++ src/styles/Project1Styles.js
@@ -0,0 +1,9 @@
+export default {
+	button: {
+		'& a': {
+			color: '#000000',
+      textDecoration: 'none',
+      textTransform: 'none'
+		}
+	}
+};
