05276eb137fe8aa0da42da1730b1e3c9bfcadf7c
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-05 12:27:07 -0500

    Refactor Database

--- src/App.js
+++ src/App.js
@@ -1,76 +1,18 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
-import Firebase from 'firebase';
-import config from './dbConfig';
+import Database from './Database';
 /*
 	Made by: GreatGameDota
 	https://github.com/GreatGameDota
 */
 
 class App extends Component {
-	state = {
-		users: []
-	};
-	constructor (props) {
-		super(props);
-		Firebase.initializeApp(config);
-	}
-	componentDidMount () {
-		this.getUserData();
-	}
-	componentDidUpdate (prevProps, prevState) {
-		if (prevState !== this.state) {
-			this.writeUserData();
-		}
-	}
-	writeUserData = () => {
-		Firebase.database().ref('/').set(this.state);
-	};
-	getUserData = () => {
-		let ref = Firebase.database().ref('/');
-		ref.on('value', (snapshot) => {
-			const state = snapshot.val();
-			this.setState(state);
-		});
-	};
-	removeData = (user) => {
-		user = this.state.users[0];
-		const { users } = this.state;
-		const newState = users.filter((data) => {
-			return data.uid !== user.uid;
-		});
-		this.setState({ users: newState });
-	};
-	addData = (data) => {
-		data = { uid: new Date().getTime().toString(), name: 'test name' };
-		this.setState((prevState) => ({
-			users: [ ...prevState.users, data ]
-		}));
-	};
-	updateData = (event) => {
-		event.preventDefault();
-		let name = this.refs.name.value;
-		let uid = this.refs.uid.value;
-		if (uid && name) {
-			const { developers } = this.state;
-			const devIndex = developers.findIndex((data) => {
-				return data.uid === uid;
-			});
-			developers[devIndex].name = name;
-			this.setState({ developers });
-		} else if (name) {
-			const uid = new Date().getTime().toString();
-			this.setState((prevState) => ({
-				developers: [ ...prevState.developers, { uid, name } ]
-			}));
-		}
-		this.refs.name.value = '';
-		this.refs.uid.value = '';
-	};
 	render () {
 		return (
 			<div>
-				<Routes db={this.state} addData={this.addData} removeData={this.removeData} />
+				<Database>
+					<Routes />
+				</Database>
 			</div>
 		);
 	}
