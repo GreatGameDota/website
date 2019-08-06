04c929c9c4033077ba49b617745bb92d0b1484aa
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-05 20:06:56 -0500

    Fix removeData function bug

--- src/Database.js
+++ src/Database.js
@@ -4,21 +4,26 @@ import config from './dbConfig';
 
 export default class Database extends Component {
 	state = config.reset;
+
 	constructor (props) {
 		super(props);
 		Firebase.initializeApp(config);
 	}
+
 	componentDidMount () {
 		this.getData();
 	}
+
 	componentDidUpdate (prevProps, prevState) {
 		if (prevState !== this.state) {
 			this.writeData();
 		}
 	}
+
 	writeData = () => {
 		Firebase.database().ref('/').set(this.state);
 	};
+
 	getData = () => {
 		let ref = Firebase.database().ref('/');
 		ref.on('value', (snapshot) => {
@@ -26,17 +31,24 @@ export default class Database extends Component {
 			this.setState(state);
 		});
 	};
+
 	addData = (data, model) => {
 		data = { uid: new Date().getTime().toString(), name: 'test name' };
 		Firebase.database().ref('/').child(model).set([ ...this.state[model], data ]);
+		// data = { uid: new Date().getTime().toString(), path: '/projects/project1', name: 'Project1' };
+		// Firebase.database().ref('/').child('projects').set([ ...this.state['projects'], data ]);
 	};
+
 	removeData = (newData, model) => {
 		newData = this.state[model][0];
-		const newState = this.state[model].filter((data) => {
+		const newArr = this.state[model].filter((data) => {
 			return data.uid !== newData.uid;
 		});
-		Firebase.database().ref('/').child(model).set(newState);
+		let newState = {};
+		newState[model] = newArr;
+		this.setState(newState);
 	};
+
 	updateData = (newData, model) => {
 		newData = { uid: '1565049730183', name: 'test update name' };
 		const devIndex = this.state[model].findIndex((data) => {
@@ -48,6 +60,7 @@ export default class Database extends Component {
 			console.log('No item with that id');
 		}
 	};
+
 	render () {
 		const children = React.Children.map(this.props.children, (child) => {
 			return React.cloneElement(child, {
