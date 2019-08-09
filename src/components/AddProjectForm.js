ea7b1c0a153488d4917f10df264d122ac69435fc
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-09 12:02:05 -0500

    Add Project link to form

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -10,7 +10,7 @@ class AddProjectForm extends Component {
 	};
 	render () {
 		const { classes } = this.props;
-		const initialValues = { name: '' };
+		const initialValues = { name: '', link: '' };
 		return (
 			<div className={classes.root}>
 				<Formik
@@ -22,6 +22,15 @@ class AddProjectForm extends Component {
 						} else if (values.name.replace(/ /g, '').length > 20) {
 							errors.name = "Name can't be longer than 20 characters";
 						}
+						if (!values.link) {
+							errors.link = 'Required';
+						} else if (
+							!/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+								values.link
+							)
+						) {
+							errors.link = 'Submit valid link';
+						}
 						return errors;
 					}}
 					onSubmit={(values, { setSubmitting, resetForm }) => {
@@ -29,12 +38,21 @@ class AddProjectForm extends Component {
 						setSubmitting(false);
 						resetForm(initialValues);
 					}}
+					validateOnBlur={false}
+					validateOnChange={false}
 				>
 					{({ isSubmitting }) => (
 						<Form>
-							<Field type='name' name='name' />
+							<label for='name'>Project Name </label>
+							<Field type='text' name='name' id='name' placeholder='Name' />
 							<span className={classes.error}>
 								<ErrorMessage name='name' />
+							</span>
+							<br />
+							<label for='link'>Project Link </label>
+							<Field type='text' name='link' id='link' placeholder='Link' />
+							<span className={classes.error}>
+								<ErrorMessage name='link' />
 							</span>
 							<br />
 							<button type='submit' disabled={isSubmitting}>
