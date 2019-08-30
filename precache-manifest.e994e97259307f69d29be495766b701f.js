06867b170188fb0b8611920f83ba6ebf33460a15
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-30 14:09:37 -0500

    Updates

---
+++ precache-manifest.e994e97259307f69d29be495766b701f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12043cec23b877eead38a9f9b1e8759",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "3e74473c91cefc6862dd",
+    "url": "/website/static/js/2.93caf325.chunk.js"
+  },
+  {
+    "revision": "1c49d37022148d816ece",
+    "url": "/website/static/js/main.82812530.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/website/static/media/github.617870e4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.068156b908bc04cabc170593120e9da1.js"
+  "/website/precache-manifest.e994e97259307f69d29be495766b701f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
