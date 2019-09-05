9396906e2a6f4019af5e5a7c6790b66ccf2ace9d
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-09-05 12:11:55 -0500

    Add initial footer contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
 import styles from '../styles/FooterStyles';
+import Link from '@material-ui/core/Link';
+import GithubIcon from '../resources/github.svg';
+import IconButton from '@material-ui/core/IconButton';
 
 class Footer extends Component {
 	render () {
 		const { classes } = this.props;
 		return (
 			<div className={classes.root}>
-				Footer
+				<Link className={classes.link} href='#'>
+					About
+				</Link>
+				<Link className={classes.link} href='#'>
+					Contact
+				</Link>
+				<br />
+				<a href='https://github.com/GreatGameDota' target='_blank' rel='noopener noreferrer' className={classes.button}>
+					<IconButton className={classes.button} aria-label='github'>
+						<img src={GithubIcon} alt='github' width='30px' height='30px' />
+					</IconButton>
+				</a>
+				<br />
+				<div className={classes.rights}>© 2019 GreatGameDota</div>
 			</div>
 		);
 	}
--- src/styles/FooterStyles.js
+++ src/styles/FooterStyles.js
@@ -1,8 +1,22 @@
 export default {
 	root: {
 		color: '#ffffff',
-		height: '50vh',
+		// height: '50vh',
+		textAlign: 'center',
 		padding: '8px',
 		backgroundColor: (props) => props.colorPrimary[500]
+	},
+	link: {
+		color: '#ffffff',
+		padding: '8px',
+		fontWeight: 'bold',
+		fontSize: '1.2rem'
+	},
+	button: {
+		padding: '3px'
+	},
+	rights: {
+		color: 'rgba(0,0,0,0.5)',
+		fontSize: '0.7rem'
 	}
 };
