28025d45b1649be3b734354b526118156ade5943
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2022-09-19 21:03:48 -0500

    Update AddProjectForm UI

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from '../styles/AddProjectFormStyles';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class AddProjectForm extends Component {
 	state = {
-		error: ''
+		errors: {}
 	};
 
 	addProject = (project) => {
@@ -14,7 +19,7 @@ class AddProjectForm extends Component {
 				if (res.ok) {
 					return res.json();
 				} else {
-					this.setState({ error: ` ${res.statusText} Invalid Project Name` });
+					this.setState({ errors: { ...this.state.errors, api: ` ${res.statusText} Invalid Project Name` } });
 					throw Error(res.statusText);
 				}
 			})
@@ -41,7 +46,7 @@ class AddProjectForm extends Component {
 						this.props.add(project, 'projects');
 					});
 			})
-			.catch((e) => console.error(`${e} - Invalid Project Name`));
+			.catch((e) => {});
 		return new Promise((res, rej) => {
 			setTimeout(function () {
 				res();
@@ -53,47 +58,68 @@ class AddProjectForm extends Component {
 		const { classes, db } = this.props;
 		const initialValues = { repo: '' };
 		return (
-			<div className={classes.root}>
+			<Box className={classes.root} borderRadius={4} boxShadow={3}>
 				<Formik
 					initialValues={initialValues}
 					validate={(values) => {
-						this.setState({ error: '' });
 						let errors = {};
-						if (!values.repo) {
+						if (values.repo.length === 0) {
 							errors.repo = ' Required';
 						}
-						if (db.projects.some(e => e.repo === values.repo)) {
+						if (db.projects.some((e) => e.repo === values.repo)) {
 							errors.repo = ' Project Already Added';
 						}
+						this.setState({ errors: errors });
 						return errors;
 					}}
 					onSubmit={(values, { setSubmitting, resetForm }) => {
 						this.addProject(values).then((result) => {
 							setSubmitting(false);
-							if (!this.state.error) {
-								resetForm(initialValues);
-							}
+							// if (Object.keys(this.state.errors).length === 0) {
+							// 	resetForm(initialValues);
+							// }
 						});
 					}}
 					validateOnBlur={false}
 					validateOnChange={false}
 				>
-					{({ isSubmitting }) => (
-						<Form>
-							<label htmlFor='repo'>Github Repo Name </label>
-							<Field type='text' name='repo' id='repo' placeholder='Repo Name' />
-							<span className={classes.error}>
-								<ErrorMessage name='repo' />
-								{this.state.error}
-							</span>
-							<br />
-							<button type='submit' disabled={isSubmitting}>
-								Submit
-							</button>
+					{({ isSubmitting, setFieldValue }) => (
+						<Form className={classes.content}>
+							<Grid container spacing={2}>
+								<Grid item sm={12}>
+									<Typography variant='h5'>
+										<label htmlFor='repo'>Add Project</label>
+									</Typography>
+									<TextField
+										name='repo'
+										id='repo'
+										label='Repo Name'
+										error={Object.keys(this.state.errors).length !== 0}
+										onChange={(event) => setFieldValue('repo', event.target.value)}
+									/>
+								</Grid>
+								<Grid item sm={12}>
+									<span className={classes.error}>
+										<ErrorMessage name='repo' />
+										{this.state.errors.api}
+									</span>
+								</Grid>
+								<Grid item sm={12}>
+									<Button
+										type='submit'
+										disabled={isSubmitting}
+										variant='outlined'
+										size='large'
+										className={classes.button}
+									>
+										Submit
+									</Button>
+								</Grid>
+							</Grid>
 						</Form>
 					)}
 				</Formik>
-			</div>
+			</Box>
 		);
 	}
 }
--- src/styles/AddProjectFormStyles.js
+++ src/styles/AddProjectFormStyles.js
@@ -1,6 +1,24 @@
-export default {
-	root: {},
+export default (theme) => ({
+	root: {
+		width: '50%',
+		height: '50vh',
+		maxWidth: '800px',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginTop: theme.spacing(3),
+		marginBottom: theme.spacing(3),
+		padding: theme.spacing(2)
+	},
+	content: {
+		marginTop: theme.spacing(10),
+		textAlign: 'center'
+	},
 	error: {
-		color: 'red'
+		color: 'red',
+		padding: '8px'
+	},
+	button: {
+		color: (props) => props.colorPrimary[500],
+		borderColor: (props) => props.colorPrimary[500]
 	}
-};
+});
